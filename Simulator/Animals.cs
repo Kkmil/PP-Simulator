e5d5699a29a25f4fa7f42234f9ce410d3cdacae3
Kkmil <kamilkufel3@gmail.com>
2024-12-12 22:36:06 +0100

    Stwory

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,9 +1,33 @@
-﻿namespace Simulator
+﻿namespace Simulator;
+
+public class Animals
 {
-    public class Animals
+    private string description = "Unknown";
+    public required string Description
     {
-        public required string Description { get; init; }
-        public uint Size { get; set; } = 3;
-        public string Info => $"{Description} <{Size}>";
+        get => description;
+        init
+        {
+            value = value.Trim();
+
+            if (value.Length > 15)
+            {
+                value = value.Substring(0, 15).TrimEnd();
+            }
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            description = value.ToUpper()[0] + value[1..value.Length];
+
+            description = value;
+        }
     }
+
+
+    public uint Size { get; set; } = 3;
+
+    public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,18 +1,56 @@
 ﻿namespace Simulator
 {
-    internal class Creature
+    public class Creature
     {
-        private string? name;
-        private int level;
-        public string? Name { get; set; }
-        public int Level { get; set; } = 1;
-        public Creature(string name, int level)
+        private string name = "Unknown";
+        private int level = 1;
+        public string Name
+        {
+            get { return name; }
+            init
+            {
+                value = value.Trim();
+                if (value.Length > 25)
+                {
+                    value = value.Substring(0, 25).TrimEnd();
+                }
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+                if (value.Length > 0 && char.IsLower(value[0]))
+                {
+                    value = char.ToUpper(value[0]) + value.Substring(1);
+                }
+                name = name.ToUpper()[0] + name[1..name.Length];
+            }
+        }
+        public int Level
+        {
+            get { return level; }
+            init
+            {
+                level = (value >= 1 && value <= 10) ? value : (value < 1 ? 1 : 10);
+            }
+        }
+
+        public Creature(string name, int level = 1)
         {
             Level = level;
-            this.Name = name;
+            Name = name;
         }
+
         public Creature() { }
+
         public void SayHi() => Console.WriteLine($"Hi, my name is {Name}, my level is {Level}");
-        public string Info() => $"{Name} {Level}";
+
+        public string Info => $"{Name} - {Level}";
+
+        public void Upgrade()
+        {
+            this.level = this.level < 10 ? ++this.level : 10;
+        }
+
+       
     }
 }
